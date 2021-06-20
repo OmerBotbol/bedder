cc7ac8bae586f14a1e1bd591af718b10a11b7ce6
noavrd <noavrd@gmail.com>
2021-06-21 00:44:45 +0300

    Show assets to renters

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,28 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { eraseCookie } from "../utils/cookies";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { eraseCookie } from '../utils/cookies';
+import ShowAsset from './ShowAsset';
 
 function HomePage({ user, setUser }) {
+  const [assets, setAssets] = useState([]);
   const logout = () => {
-    eraseCookie("accessToken");
-    eraseCookie("refreshToken");
-    setUser("");
+    eraseCookie('accessToken');
+    eraseCookie('refreshToken');
+    setUser('');
   };
 
+  const getAssets = () => {
+    axios
+      .get('/api/asset')
+      .then((data) => {
+        console.log(data.data);
+        setAssets(data.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getAssets();
+  }, []);
   return (
     <>
       <h1>Home Page</h1>
       {user ? (
         <>
           <div>{user.email}</div>
-          <div>{user.isOwner ? "Owner" : "Renter"}</div>
+          <div>{user.isOwner ? 'Owner' : 'Renter'}</div>
           <button
             onClick={() => {
               logout();
-            }}
-          >
+            }}>
             logout
           </button>
+          {user.isOwner ? (
+            ''
+          ) : (
+            <div>
+              {assets.map((asset, i) => (
+                <ShowAsset key={i} asset={asset} />
+              ))}
+            </div>
+          )}
         </>
       ) : (
         <>
---
+++ client/src/components/ShowAsset.js
@@ -0,0 +1,14 @@
+export default function ShowAsset({ asset }) {
+  return (
+    <div className="asset">
+      {console.log(1)}
+      <span>{asset.city}</span>
+      <p>{asset.description}</p>
+      <p>Availability</p>
+      <p>
+        {asset.started_at.slice(0, 10).replaceAll('-', '/')}-
+        {asset.ended_at.slice(0, 10).replaceAll('-', '/')}
+      </p>
+    </div>
+  );
+}
