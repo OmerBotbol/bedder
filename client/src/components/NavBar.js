8c2ddbf01bcf15303282dd2630f9394275bb0df5
noavrd <noavrd@gmail.com>
2021-06-28 23:58:51 +0300

    style navbar (unfinished)

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { eraseCookie } from "../utils/cookies";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { eraseCookie } from '../utils/cookies';
+import axios from 'axios';
+import '../styles/navbar.css';
 
 function NavBar({ user, setUser }) {
   const [userDetails, setUserDetails] = useState({});
 
   useEffect(() => {
     if (user) {
-      const route = user.isOwner ? "owner" : "renter";
+      const route = user.isOwner ? 'owner' : 'renter';
       axios
         .get(`/api/${route}/${user.id}`)
         .then((data) => setUserDetails(data.data))
@@ -17,29 +18,49 @@ function NavBar({ user, setUser }) {
   }, [user]);
 
   const logout = () => {
-    eraseCookie("accessToken");
-    eraseCookie("refreshToken");
-    setUser("");
+    eraseCookie('accessToken');
+    eraseCookie('refreshToken');
+    setUser('');
   };
 
   return (
-    <div>
-      <i
-        className={`fa fa-user-circle${user.isOwner ? "-o" : ""}`}
-        aria-hidden="true"
-      ></i>
-      <span> </span>
-      <Link to="/profile">
-        {userDetails.first_name} {userDetails.last_name}
-      </Link>
-      <button
-        onClick={() => {
-          logout();
-        }}
-      >
-        logout
-      </button>
-    </div>
+    <ul className="navbar">
+      <li className="dropbtn dropdown">
+        <i
+          className={`fa fa-user-circle${user.isOwner ? '-o' : ''}`}
+          aria-hidden="true"></i>
+
+        <span className="dropdown">
+          {userDetails.first_name} {userDetails.last_name}
+        </span>
+        <span> </span>
+        <i className="fa fa-caret-down"></i>
+
+        <div className="dropdown-content">
+          <div className="dropdown-link">
+            <Link to="/profile">Profile</Link>
+          </div>
+
+          <div
+            className="dropdown-link"
+            onClick={() => {
+              logout();
+            }}>
+            logout
+          </div>
+        </div>
+        <span></span>
+      </li>
+      <li className="other-links">My Assets</li>
+      <li className="other-links">Likes</li>
+      <li className="other-links">My orders</li>
+      <li>
+        <Link to="/">
+          <i className="fa fa-fw fa-home" />
+        </Link>
+      </li>
+      <li className="navbar-title">Bedder</li>
+    </ul>
   );
 }
 
