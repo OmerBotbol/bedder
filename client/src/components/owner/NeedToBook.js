e8cced21acd6a851204d50d39e5417eb5ba34316
noavrd <noavrd@gmail.com>
2021-06-30 13:57:06 +0300

    show owner renter details after like

--- client/src/components/owner/NeedToBook.js
+++ client/src/components/owner/NeedToBook.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function NeedToBook({ offer, setNeedToBook, needToBook }) {
+  const [renter, setRenter] = useState({});
+  useEffect(() => {
+    renterDetails();
+  }, []);
+
+  console.log(renter);
   const refreshNeedToBook = () => {
     const offersCopy = [...needToBook];
     const index = offersCopy.findIndex((option) => {
@@ -9,6 +15,12 @@ function NeedToBook({ offer, setNeedToBook, needToBook }) {
     });
     offersCopy.splice(index, 1);
     setNeedToBook(offersCopy);
+  };
+  const renterDetails = () => {
+    axios
+      .get(`/api/renter/${offer.renter_id}`)
+      .then((data) => setRenter(data.data))
+      .catch((err) => console.log(err));
   };
   const book = () => {
     const dataToSend = {
@@ -32,11 +44,18 @@ function NeedToBook({ offer, setNeedToBook, needToBook }) {
       })
       .catch((err) => console.log(err));
   };
+  console.log(offer);
 
   return (
     <div>
       <p>start Date: {offer.started_at.slice(0, 10)}</p>
       <p>end Date: {offer.ended_at.slice(0, 10)}</p>
+      <h3>Renter Details:</h3>
+      <p>
+        {renter.first_name} {renter.last_name}
+      </p>
+      <p>{renter.email}</p>
+      <p>{renter.phone_number}</p>
       <button onClick={() => book()}>Book!</button>
       <button onClick={() => deleteTransaction()}>Cancel</button>
     </div>
