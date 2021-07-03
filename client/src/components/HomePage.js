8693dde3fb3c505c7f0e07d922bce7d360de17e4
Shira-Meirovitz <shirameirovi@gmail.com>
2021-07-03 05:54:30 +0300

    css for story

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -50,9 +50,33 @@ function HomePage({ user }) {
               <Popup trigger={<button>About Us</button>} position="top left">
                 {(close) => (
                   <div className="box">
+                    <div className="header">
+                      <h1 className="story"> Our Story</h1>
+                    </div>
                     <div className="content">
-                      <h1> Our Story</h1>
-                      Content here
+                      <p>
+                        <p>
+                          This application was established in 2021 immediately
+                          after the Operation Guardian of the Walls, by Omar
+                          Botbul, Shira Meirovitz and Noa Shalom as part of a
+                          program for veterans that operated by Scale-Up
+                          Velocity in cooperation with the IDF and the cyber
+                          system.
+                        </p>
+                        The purpose of the application is to reflect the
+                        knowledge acquired after five months of training and has
+                        one vision: to create and expand the circles of
+                        volunteering and giving in Israeli society, with an
+                        emphasis on free accommodation. In order to promote this
+                        vision, we have developed an application that makes
+                        accessible and quick search for accommodation, as well
+                        as advertising accommodation at the click of a button.
+                      </p>
+                      <p>
+                        We believe that volunteering can and should become
+                        public domain, and that the way there is through quality
+                        technology.
+                      </p>
                       <a className="close" onClick={close}>
                         &times;
                       </a>
