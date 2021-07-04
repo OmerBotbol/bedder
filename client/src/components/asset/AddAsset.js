399f185953bd21f11d6bd1739abdd5e54c3a9810
noavrd <noavrd@gmail.com>
2021-07-04 23:03:47 +0300

    start add-asset css

--- client/src/components/asset/AddAsset.js
+++ client/src/components/asset/AddAsset.js
@@ -4,6 +4,7 @@ import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
 import { DateRange } from 'react-date-range';
 import { deleteHttp, postHttp } from '../../utils/httpRequests';
+import '../../styles/addAsset.css';
 
 export default function AddAsset({ user }) {
   const [city, setCity] = useState('');
@@ -92,89 +93,158 @@ export default function AddAsset({ user }) {
   };
 
   return (
-    <div>
-      <h1>Add asset</h1>
-      <label>City</label>
-      <input type="text" onChange={(e) => setCity(e.target.value)} />
-      <br />
-      <label>Address</label>
-      <input type="text" onChange={(e) => setAddress(e.target.value)} />
-      <br />
+    <div id="add-asset-container">
+      <h1 id="add-asset-header">Add asset</h1>
+      <div className="form">
+        <div className="form-field">
+          <label className="add-asset-label">City</label>
+          <input
+            type="text"
+            className="add-asset-text"
+            onChange={(e) => setCity(e.target.value)}
+          />
+        </div>
+        <div className="form-field">
+          <label className="add-asset-label">Address</label>
+          <input
+            type="text"
+            className="add-asset-text"
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </div>
+        <div className="form-field">
+          <label className="add-asset-label">Description</label>
+          <input
+            className="add-asset-text"
+            type="text"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="form-field">
+          <label className="add-asset-label">Room Type</label>
+          <select
+            className="add-asset-text"
+            onChange={(e) => setRoomType(e.target.value)}>
+            <option value="1">Apartment</option>
+            <option value="2">Couch</option>
+            <option value="3">Separate room</option>
+            <option value="4">Shared room</option>
+            <option value="5">Tent</option>
+          </select>
+        </div>
+        <div className="form-field">
+          <label className="add-asset-label">Hospitality</label>
+          <select
+            className="add-asset-text"
+            onChange={(e) => setHospitality(e.target.value)}>
+            <option value="1">Full</option>
+            <option value="2">Only sleep</option>
+          </select>
+        </div>
 
-      <label>Room Type</label>
-      <select onChange={(e) => setRoomType(e.target.value)}>
-        <option value="1">Apartment</option>
-        <option value="2">Couch</option>
-        <option value="3">Separate room</option>
-        <option value="4">Shared room</option>
-        <option value="5">Tent</option>
-      </select>
-      <br />
-      <label>Hospitality</label>
-      <select onChange={(e) => setHospitality(e.target.value)}>
-        <option value="1">Full</option>
-        <option value="2">Only sleep</option>
-      </select>
-      <br />
+        <div className="form-field">
+          <label className="add-asset-label">Number of people</label>
+          <input
+            className="add-asset-text"
+            type="number"
+            min="1"
+            onChange={(e) => setNumberOfPeople(e.target.value)}
+          />
+        </div>
+        <div className="form-field">
+          <label className="add-asset-label">Number of rooms</label>
+          <input
+            className="add-asset-text"
+            type="number"
+            min="1"
+            onChange={(e) => setNumberOfRooms(e.target.value)}
+          />
+        </div>
+        <div className="form-field">
+          <label className="add-asset-label">Picture</label>
+          <input
+            className="add-asset-text"
+            type="file"
+            accept="image/*"
+            onChange={(e) => setPicture(e.target.files[0])}
+          />
+        </div>
+        <div className="form-field">
+          <label className="add-asset-label">Kosher</label>
+          <input
+            className="option-input radio"
+            type="checkbox"
+            onChange={() => setKosher(!kosher)}
+          />
+        </div>
+        <div className="form-field">
+          <label className="add-asset-label">Shabbat</label>
+          <input
+            type="checkbox"
+            className="option-input radio"
+            onChange={() => setShabbat(!shabbat)}
+          />
+        </div>
+        <div className="form-field">
+          <label className="add-asset-label">Parking</label>
+          <input
+            type="checkbox"
+            className="option-input radio"
+            onChange={() => setParking(!parking)}
+          />
+        </div>
+        <div className="form-field">
+          <label className="add-asset-label">Animals</label>
+          <input
+            type="checkbox"
+            className="option-input radio"
+            onChange={() => setAnimals(!animals)}
+          />
+        </div>
+        <div className="form-field">
+          <label className="add-asset-label">AC</label>
+          <input
+            type="checkbox"
+            className="option-input radio"
+            onChange={() => setAC(!AC)}
+          />
+        </div>
+        <div className="form-field">
+          <label className="add-asset-label">Accessibility</label>
+          <input
+            className="option-input radio"
+            type="checkbox"
+            onChange={(e) => setAccessibility(!accessibility)}
+          />
+        </div>
+        <div className="form-field">
+          <label className="add-asset-label">Babies</label>
+          <input
+            type="checkbox"
+            className="option-input radio"
+            onChange={() => setBabies(!babies)}
+          />
+        </div>
 
-      <label>Description</label>
-      <input type="email" onChange={(e) => setDescription(e.target.value)} />
-      <br />
-      <label>Number of people</label>
-      <input
-        type="number"
-        onChange={(e) => setNumberOfPeople(e.target.value)}
-      />
-      <br />
-      <label>Number of rooms</label>
-      <input type="number" onChange={(e) => setNumberOfRooms(e.target.value)} />
-      <br />
-      <label>Kosher</label>
-      <input type="checkbox" onChange={() => setKosher(!kosher)} />
-      <br />
-      <label>Shabbat</label>
-      <input type="checkbox" onChange={() => setShabbat(!shabbat)} />
-      <br />
-      <label>Parking</label>
-      <input type="checkbox" onChange={() => setParking(!parking)} />
-      <br />
-      <label>Animals</label>
-      <input type="checkbox" onChange={() => setAnimals(!animals)} />
-      <br />
-      <label>AC</label>
-      <input type="checkbox" onChange={() => setAC(!AC)} />
-      <br />
-      <label>Accessibility</label>
-      <input
-        type="checkbox"
-        onChange={(e) => setAccessibility(!accessibility)}
-      />
-      <br />
-      <label>Babies</label>
-      <input type="checkbox" onChange={() => setBabies(!babies)} />
-      <br />
-      <label>Picture</label>
-      <input
-        type="file"
-        accept="image/*"
-        onChange={(e) => setPicture(e.target.files[0])}
-      />
-      <br />
-      <DateRange
-        editableDateInputs={true}
-        moveRangeOnFirstSelection={false}
-        onChange={(i) => handleSelect(i)}
-        ranges={[selectionRange]}
-        minDate={new Date()}
-      />
-      <br />
+        <div className="form-field">
+          <label className="add-asset-label">Available dates</label>
+          <DateRange
+            editableDateInputs={true}
+            moveRangeOnFirstSelection={false}
+            onChange={(i) => handleSelect(i)}
+            ranges={[selectionRange]}
+            minDate={new Date()}
+          />
+        </div>
+      </div>
       <button
+        className="add-asset-btn"
         onClick={() => {
           handleClick();
         }}>
         Add
       </button>
-      {message && <p>{message}</p>}
+      {message && <p className="error">{message}</p>}
       {redirect && <Redirect to="/login" />}
     </div>
   );
