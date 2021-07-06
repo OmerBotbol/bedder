4dd169f493104103e2334bfd5cc35187c1a90c31
Shira-Meirovitz <shirameirovi@gmail.com>
2021-07-06 21:51:00 +0300

    started gps

--- client/src/components/renter/RenterHomePage.js
+++ client/src/components/renter/RenterHomePage.js
@@ -64,6 +64,13 @@ function RenterHomePage({ user }) {
             }, 1fr)`,
           }}
         >
+          <div id="map"></div>
+          <script
+            async
+            defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBwiV5ssJ3sw79n3pHDAosob46P5wIw0F0&callback=initMap"
+          ></script>
+          {initMap()}
           <Search
             searchInput={searchInput}
             setSearchInput={setSearchInput}
@@ -129,6 +136,67 @@ function RenterHomePage({ user }) {
       </div>
     </div>
   );
+}
+
+var map, infoWindow;
+function initMap() {
+  const google = window.google;
+  // geocoder = new google.maps.Geocoder();
+  var geocoder = new google.maps.Geocoder();
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 6,
+  });
+  infoWindow = new google.maps.InfoWindow();
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+
+        geocoder.geocode(
+          { location: pos },
+          function (results, status, infowindow) {
+            if (status == 'OK') {
+              console.log(results[0].formatted_address);
+              infoWindow.setContent(
+                'Location found: ' + results[0].formatted_address
+              );
+              infoWindow.setPosition(pos);
+              infoWindow.open(map);
+            } else {
+              console.log(
+                'Geocode was not successful for the following reason: ' + status
+              );
+            }
+          }
+        );
+
+        map.setCenter(pos);
+      },
+      function () {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? 'Error: The Geolocation service failed.'
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
 
 export default RenterHomePage;
